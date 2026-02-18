490a6831904850cee1c8a8eba0242d69273de999
Filip Vagner <filip.vagner@orbit.cz>
2026-02-18 20:12:02 +0100

    Add output for container registry images

--- ouputs.tf
+++ ouputs.tf
@@ -14,3 +14,15 @@ output "admin_password" {
   value = azurerm_container_registry.this.admin_password
   sensitive = true
 }
+
+output "images" {
+  value = {
+    for key, image in var.container_registry_images :
+    key => {
+      task_name       = image.task_name
+      dockerfile_path = image.dockerfile_path
+      context_path    = image.context_path
+      image_names     = image.image_names
+    }
+  }
+}
